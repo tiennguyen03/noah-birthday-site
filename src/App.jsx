2ad2ec5b60cd116b2e5447035a8fe53321bc70c8
Tien Nguyen <tiennguyenbusiness@gmail.com>
2025-09-10 16:08:50 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='center-text'>
+        <h1 className="title">Happy Birthday</h1>
+      <h1>Noah's 21st Birthday</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
