b1265414ccf550e184043e70c40e748c45043703
Tien Nguyen <tiennguyenbusiness@gmail.com>
2025-09-11 18:08:57 -0400

    Playlists Added

--- src/assets/components/Playlist.jsx
+++ src/assets/components/Playlist.jsx
@@ -1,9 +1,31 @@
 function Playlist() {
-    return(
-        <>
-            <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6SkdwwFZpY8g8GWcAGrAmt?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </>
-    );
-};
+  return (
+    <>
+      <div className="pl-container">
 
-export default Playlist; 
+        <h1>Background Playlist</h1>
+        <iframe
+          className="party-playlist"
+          src="https://open.spotify.com/embed/playlist/6SkdwwFZpY8g8GWcAGrAmt?utm_source=generator"
+          height="450"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          allowFullScreen
+        ></iframe>
+
+
+        <h1>Karaoke Playlist</h1>
+        <iframe
+          className="party-playlist"
+          src="https://open.spotify.com/embed/playlist/4aCDNIqf0XzYmhzT8CUGJa?utm_source=generator"
+          height="450"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </>
+  );
+}
+
+export default Playlist;
