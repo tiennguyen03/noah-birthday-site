053b50cae6ca67a68362ff918b24337e2bd69f02
Tien Nguyen <tiennguyenbusiness@gmail.com>
2025-09-13 16:25:47 -0400

    Small Fix

--- src/assets/components/Gift.jsx
+++ src/assets/components/Gift.jsx
@@ -70,7 +70,7 @@ function Gift() {
 
         {unlocked && (
           <div className="gift-reveal">
-            <h2>AQGV-XP4SJ2-MMEAV</h2>
+            <h2>AMAZON: AQGV-XP4SJ2-MMEAV</h2>
           </div>
         )}
       </div>
